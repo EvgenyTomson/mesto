4c5dd3448f74f4569e1d92ce465a703bf4ab3d45
EvgenyTomson <tomilin.evgeny@gmail.com>
2022-11-21 21:03:29 +0300

    fix: ревью 2 найти правильный элемент формы (JS)

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__about');
 
 const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__container');
+const popupForm = document.querySelector('.popup__form');
 const inputName = document.querySelector('#inputName');
 const inputJob = document.querySelector('#inputJob');
 const closeBtn = document.querySelector('.popup__close');
